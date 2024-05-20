0fd69a945a586eb6fa2ac94a18144bcf4cd698bf
kailam-ada <kai_lam@hotmail.fr>
2024-05-20 16:04:10 +0200

    NavLink iterating through each stationName of stations

--- src/components/TrainStations.jsx
+++ src/components/TrainStations.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
 
-function TrainStations() {
+function TrainStations({ stations }) {
   return (
-    <div>TrainStations</div>
+    <div className="train-stations">
+        { Object.keys(stations).map((stationName) => (
+            <NavLink 
+            className={({ isActive }) => 
+                `train-stations__link ${ 
+                    isActive ? 'train-stations__link--active' : ''
+
+                }`
+            } 
+            key={stationName}
+            to={`${stations[stationName]}`}
+            >
+                <span>{stationName}</span>
+            </NavLink>
+        ))}
+    </div>
   )
 }
 
