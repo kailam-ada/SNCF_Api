b4512068799c621cd8133f569e40ed2de51a6a9d
kailam-ada <kai_lam@hotmail.fr>
2024-05-20 16:33:00 +0200

    created buttons in TrainStation and new Departures component

---
+++ src/components/Departures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Departures() {
+  return (
+    <div>Departures</div>
+  )
+}
+
+export default Departures
--- src/components/TrainStation.jsx
+++ src/components/TrainStation.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react"
+import Departures from "./Departures"
+
 function TrainStation() {
+    const [departureMode, setDepartureMode] = useState(true)
   return (
-    <div>TrainStation</div>
+    <div className="train-station">
+        <div className="directions">
+            <button type="button" 
+            className={`directions__departures ${departureMode ? 'directions__departures--active' : ''}`} 
+            onClick={() => setDepartureMode(true)}
+            >
+                Départs
+            </button>
+            <button type="button" 
+            className={`directions__arrivals ${departureMode ? '' : 'directions__arrivals--active'}`} 
+            onClick={() => setDepartureMode(false)}
+            >
+                Arrivées
+            </button>
+        </div>
+        {departureMode && <Departures />}
+    </div>
   )
 }
 
